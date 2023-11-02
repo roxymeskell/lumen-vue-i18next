9c1b2c51c8fb29b50c8e474e7170da90ab3cef7c
Roxy Meskell <roxy.meskell@gmail.com>
2023-11-02 13:33:14 +0000

    Route working

--- api/app/Http/Controllers/I18nController.php
+++ api/app/Http/Controllers/I18nController.php
@@ -19,9 +19,17 @@ class I18nController extends Controller
     public function get($key)
     {
         $locale = 'en';
-        $i18n = I18n::findOrFail($key);
+        $i18n = I18n::where('key', '=', $key)->firstOrFail();
         // $translation = $i18n->translations()->with('locale')->where('locale', $locale)->first();
-        $translation = $i18n->translations()->with('locale')->first();
+        // $translation = $i18n->translations()->with('locale')->first();
+
+        $translation = $i18n->translations()->whereHas('locale', function ($query) use ($locale) {
+            $query->where('key','=', $locale);
+        })->first();
+
+        // $translation = $i18n->translations()->with(['locale' => function ($query) use ($locale) {
+        //     $query->where('key','=', $locale);
+        // }])->first();
 
         \Illuminate\Support\Facades\Log::info('Getting translation ' . json_encode($translation));
 
